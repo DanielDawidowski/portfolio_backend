ad04621f00dc8ebd56f92b4f37d38fcc9c284d21
daniel <dvds1987@gmail.com>
2023-09-24 18:28:40 +0200

    Initialize server

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Express } from "express";
+import { PortfolioServer } from "./setupServer";
+import { config } from "./config";
+
+class Application {
+  public initialize(): void {
+    this.loadConfig();
+    const app: Express = express();
+    const server: PortfolioServer = new PortfolioServer(app);
+    server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
+  }
+}
+
+const application: Application = new Application();
+application.initialize();
---
+++ src/config.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+import bunyan from "bunyan";
+
+dotenv.config({});
+
+class Config {
+  public JWT_TOKEN: string | undefined;
+  public NODE_ENV: string | undefined;
+  public SECRET_KEY_ONE: string | undefined;
+  public SECRET_KEY_TWO: string | undefined;
+  public CLIENT_URL: string | undefined;
+  public SENDER_EMAIL: string | undefined;
+  public SENDER_EMAIL_PASSWORD: string | undefined;
+
+  constructor() {
+    this.JWT_TOKEN = process.env.JWT_TOKEN || "12345";
+    this.NODE_ENV = process.env.NODE_ENV || "";
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || "";
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
+    this.CLIENT_URL = process.env.CLIENT_URL || "";
+    this.SENDER_EMAIL = process.env.SENDER_EMAIL || "";
+    this.SENDER_EMAIL_PASSWORD = process.env.SENDER_EMAIL_PASSWORD || "";
+  }
+
+  public createLogger(name: string): bunyan {
+    return bunyan.createLogger({ name, level: "debug" });
+  }
+
+  public validateConfig(): void {
+    for (const [key, value] of Object.entries(this)) {
+      if (value === undefined) {
+        throw new Error(`Configuration ${key} is undefined`);
+      }
+    }
+  }
+}
+
+export const config: Config = new Config();
