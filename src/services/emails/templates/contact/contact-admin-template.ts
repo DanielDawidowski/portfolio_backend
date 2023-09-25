b2d7c0d89ae994ce96f90fd199cd52b05f7ee2fa
daniel <dvds1987@gmail.com>
2023-09-25 13:33:30 +0200

    Edit email template

--- src/services/emails/templates/contact/contact-admin-template.ts
+++ src/services/emails/templates/contact/contact-admin-template.ts
@@ -6,7 +6,7 @@ class ContactAdminTemplate {
   public sendMessageToAdmin(templateParams: IContactTemplate): string {
     const { name, email, message } = templateParams;
 
-    return ejs.render(fs.readFileSync(__dirname + "/contact-admin-template.ejs", "utf8"), {
+    return ejs.render(fs.readFileSync(__dirname + "/contact-admin-template.ejs", { encoding: "utf8" }), {
       name,
       email,
       message
