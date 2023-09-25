65be946f6330b636ec547ebe7c49cc4425c3a35c
daniel <dvds1987@gmail.com>
2023-09-25 15:04:35 +0200

    Edit email template

--- src/services/emails/mail.transport.ts
+++ src/services/emails/mail.transport.ts
@@ -3,7 +3,6 @@ import Mail from "nodemailer/lib/mailer";
 import Logger from "bunyan";
 import { config } from "@root/config";
 import { BadRequestError } from "@root/helpers/error-handler";
-import { IContactTemplate } from "@email/interface/email.interface";
 
 interface IMailOptions {
   from: string;
@@ -15,12 +14,11 @@ interface IMailOptions {
 const log: Logger = config.createLogger("mailOptions");
 
 class MailTransport {
-  public async sendEmail(receiverEmail: string, subject: string, body: IContactTemplate): Promise<void> {
+  public async sendEmail(receiverEmail: string, subject: string, body: string): Promise<void> {
     this.developmentEmailSender(receiverEmail, subject, body);
   }
 
-  private async developmentEmailSender(receiverEmail: string, subject: string, body: IContactTemplate): Promise<void> {
-    const { name, email, message } = body;
+  private async developmentEmailSender(receiverEmail: string, subject: string, body: string): Promise<void> {
     const transporter: Mail = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
@@ -39,12 +37,7 @@ class MailTransport {
       from: `Aktywne Obozy <${config.SENDER_EMAIL!}>`,
       to: receiverEmail,
       subject,
-      html: `
-      <h4><bold>Email:</bold>  ${email}</h4>
-      <h4><bold>Pytanie od:</bold>  ${name}</h4>
-      <p><bold>Wiadomość:</bold> ${message}</p>
-      <hr />
-  `
+      html: body
     };
 
     try {
@@ -58,3 +51,64 @@ class MailTransport {
 }
 
 export const mailTransport: MailTransport = new MailTransport();
+
+// import nodemailer from "nodemailer";
+// import Mail from "nodemailer/lib/mailer";
+// import Logger from "bunyan";
+// import { config } from "@root/config";
+// import { BadRequestError } from "@root/helpers/error-handler";
+// import { IContactTemplate } from "@email/interface/email.interface";
+
+// interface IMailOptions {
+//   from: string;
+//   to: string;
+//   subject: string;
+//   html: string;
+// }
+
+// const log: Logger = config.createLogger("mailOptions");
+
+// class MailTransport {
+//   public async sendEmail(receiverEmail: string, subject: string, body: IContactTemplate): Promise<void> {
+//     this.developmentEmailSender(receiverEmail, subject, body);
+//   }
+
+//   private async developmentEmailSender(receiverEmail: string, subject: string, body: IContactTemplate): Promise<void> {
+//     const { name, email, message } = body;
+//     const transporter: Mail = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 587,
+//       secure: false,
+//       requireTLS: true,
+//       auth: {
+//         user: process.env.SENDER_EMAIL, // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+//         pass: process.env.SENDER_EMAIL_PASSWORD // MAKE SURE THIS PASSWORD IS YOUR GMAIL APP PASSWORD WHICH YOU GENERATED EARLIER
+//       },
+//       tls: {
+//         ciphers: "SSLv3"
+//       }
+//     });
+
+//     const mailOptions: IMailOptions = {
+//       from: `Aktywne Obozy <${config.SENDER_EMAIL!}>`,
+//       to: receiverEmail,
+//       subject,
+//       html: `
+//       <h4><bold>Email:</bold>  ${email}</h4>
+//       <h4><bold>Pytanie od:</bold>  ${name}</h4>
+//       <p><bold>Wiadomość:</bold> ${message}</p>
+//       <hr />
+//   `
+//     };
+
+//     try {
+//       await transporter.sendMail(mailOptions);
+//       log.info("Development email sent successfully.");
+//     } catch (error) {
+//       log.error("Error sending email", error);
+//       throw new BadRequestError("Error sending email");
+//     }
+//   }
+// }
+
+// export const mailTransport: MailTransport = new MailTransport();
--- src/services/emails/templates/contact/contact-admin-template.ts
+++ src/services/emails/templates/contact/contact-admin-template.ts
@@ -6,11 +6,11 @@ class ContactAdminTemplate {
   public sendMessageToAdmin(templateParams: IContactTemplate): string {
     const { name, email, message } = templateParams;
 
-    return ejs.render(fs.readFileSync(__dirname + "/contact-admin-template.ejs", "utf8"), {
-      name,
-      email,
-      message
-    });
+    const str = fs.readFileSync(__dirname + "/contact-admin-template.ejs", "utf8");
+
+    const template = ejs.compile(str, { filename: __dirname + "/contact-admin-template.ejs" });
+
+    return template({ name, email, message });
   }
 }
 
